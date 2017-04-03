ee6e4355947977d811c5325449913014a3cf6168
spences10 <spences10apps@gmail.com>
2017-04-03 20:33:41 +0100

    :sparkles:defaultText in inital `state`:sparkles:

--- src/Markdown.js
+++ src/Markdown.js
@@ -1,54 +1,47 @@
 import React from 'react'
 import Marked from 'marked'
 
-const defaultText = getDefaultText()
-
 class Markdown extends React.Component {
-  constructor(props) {
-    super(props)
-    this.onChange = this.onChange.bind(this)
-  }
-  onChange(e){
-    this.setState({ source: e.target.value })
-  }
-  getMarkdownText=(evaluate) => {
-    var rawMarkup = Marked(evaluate,{sanitize: true})
-    return{
-      __html: rawMarkup
+  constructor() {
+    super()
+    this.state = {
+      defaultText: '# hello world'
     }
   }
+
+  makeMarkup(value) {
+    const convert = Marked(value)
+    return { __html: convert }
+  }
+
+  onChange(e) {
+    this.setState({ defaultText: e.target.value })
+  }
   render() {
-    let evaluate = this.state ? this.state.source : defaultText
     return (
       <div>
         <div className="row">
           <div className="col-md-6">
             <h2 className="appHeading">Markdown notiation</h2>
-            <textarea 
+            <textarea
               className="editor"
-              defaultValue={defaultText}
-              onChange={this.onChange}
-            >
-            </textarea>
+              rows="25"
+              cols="50"
+              value={this.state.defaultText}
+              onChange={this.onChange.bind(this)}
+            />
           </div>
           <divcol-md-6 renderedMarkdown>
             <h2 className="appHeading">Rendered Markdown</h2>
-            <div 
+            <div
               contentEditable="true"
-              dangerouslySetInnerHTML={this.getMarkdownText(evaluate)}
-            /> 
+              dangerouslySetInnerHTML={this.makeMarkup(this.state.defaultText)}
+            />
           </divcol-md-6>
         </div>
       </div>
     )
   }
-}
-
-function getDefaultText(){
-  return [
-    '# H1',
-    '---',
-  ].join('\n')
 }
 
 export default Markdown
