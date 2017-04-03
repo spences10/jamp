12d67c9d41f27b6320dd7e667e038883c01f3d52
spences10 <spences10apps@gmail.com>
2017-04-03 19:41:59 +0100

    :rocket:add in marked:rocket:

--- src/Markdown.js
+++ src/Markdown.js
@@ -1,30 +1,54 @@
 import React from 'react'
+import Marked from 'marked'
+
+const defaultText = getDefaultText()
 
 class Markdown extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      value: `hello worls`
+  constructor(props) {
+    super(props)
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange(e){
+    this.setState({ source: e.target.value })
+  }
+  getMarkdownText=(evaluate) => {
+    var rawMarkup = Marked(evaluate,{sanitize: true})
+    return{
+      __html: rawMarkup
     }
   }
   render() {
+    let evaluate = this.state ? this.state.source : defaultText
     return (
       <div>
         <div className="row">
           <div className="col-md-6">
             <h2 className="appHeading">Markdown notiation</h2>
-            <textarea >
-              {this.props.value}
+            <textarea 
+              className="editor"
+              defaultValue={defaultText}
+              onChange={this.onChange}
+            >
             </textarea>
           </div>
           <divcol-md-6 renderedMarkdown>
             <h2 className="appHeading">Rendered Markdown</h2>
-            <div contentEditable="true"></div>
+            <div 
+              contentEditable="true"
+              dangerouslySetInnerHTML={this.getMarkdownText(evaluate)}
+            /> 
           </divcol-md-6>
         </div>
       </div>
     )
   }
+}
+
+function getDefaultText(){
+  return [
+    '# H1',
+    '---',
+  ].join('\n')
 }
 
 export default Markdown
