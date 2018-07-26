c9d28c8190478117baa6de9356a906efba775a51
spences10 <spences10apps@gmail.com>
2018-07-26 06:52:32 +0100

    md2jsx added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import Markdown from './Markdown'
+import Markdown from 'markdown-to-jsx'
 
 class App extends Component {
+  state = { markdown: '# yo!' }
+
   render() {
     return (
       <React.Fragment>
-        <Markdown />
+        <demo>
+          <textarea
+            onInput={this.updateState}
+            value={this.state.markdown}
+          />
+
+          <compiled>
+            <Markdown>{this.state.markdown}</Markdown>
+          </compiled>
+        </demo>
       </React.Fragment>
     )
   }
