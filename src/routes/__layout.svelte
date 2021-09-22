33cd76c9d9dbacee7cb8a4055d8b062e4ce5424e
spences10 <spences10apps@gmail.com>
2021-09-22 09:29:06 +0100

    feat: :sparkles: add theme change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,13 @@
 <script>
   import Footer from '$lib/footer.svelte'
   import Header from '$lib/header.svelte'
+  import { onMount } from 'svelte'
+  import { themeChange } from 'theme-change'
   import '../app.css'
+
+  onMount(async () => {
+    themeChange(false)
+  })
 </script>
 
 <Header />
