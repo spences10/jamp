c22d79f8161d9c4a8850ef263b57e99fb8874dfa
spences10 <spences10apps@gmail.com>
2021-09-22 09:05:56 +0100

    feat: :sparkles: add markdown parser

---
+++ src/lib/markdown-parser.svelte
@@ -0,0 +1,20 @@
+<script>
+  import marked from 'marked'
+  export let markdownContent = `# Hello World\n\nWrite markdown here\n\n`
+  export let rows = 5
+  $: markdown = marked(markdownContent)
+</script>
+
+<div
+  class="mx-auto w-10/12 h-screen mb-10 grid p-10 gap-4 prose prose-lg md:gap-10 md:grid-cols-2 md:grid-rows-1"
+>
+  <textarea
+    {rows}
+    bind:value={markdownContent}
+    class="border rounded-xl h-full bg-base-200 border-base-100 text-xl w-full p-2 resize-none "
+  />
+
+  <div class="bg-neutral border border-neutral rounded-xl w-full p-2">
+    {@html markdown}
+  </div>
+</div>
