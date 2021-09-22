2daf0a6d38dd2435e5fe0eb514f75d58db8bca7f
spences10 <spences10apps@gmail.com>
2021-09-22 09:07:23 +0100

    feat: :sparkles: layout with components, add component to index

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
-  import '../app.postcss'
+  import Footer from '$lib/footer.svelte'
+  import Header from '$lib/header.svelte'
+  import '../app.css'
 </script>
 
+<Header />
 <slot />
+<Footer />
